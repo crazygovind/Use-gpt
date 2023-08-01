0b25f53b6283c7209b0b8c9840f9d3bbd4af54a1
Govind Sitaphale <sitaphalegovind.3@gmail.com>
2023-08-01 12:35:37 +0530

    Add files via upload

---
+++ your-script.js
@@ -0,0 +1,21 @@
+const promptInput = document.getElementById("prompt");
+const results = document.getElementById("results");
+
+// Initialize the OpenAI SDK with your API key
+const openai = new OpenAI('a151776f6bmsh94ea1567cbe783dp1d803bjsn21d00779e249');
+
+async function callGPTAPI() {
+    const message = [
+        { 'role': 'system', 'content': 'You are a helpful assistant.' },
+        { 'role': 'user', 'content': promptInput.value }
+    ];
+
+    try {
+        const response = await openai.complete('gpt-3.5-turbo', message);
+        console.log(response);
+        // Display the chatbot's response inside the results div
+        results.innerText = response.data.choices[0].message.content;
+    } catch (error) {
+        console.error(error);
+    }
+}
